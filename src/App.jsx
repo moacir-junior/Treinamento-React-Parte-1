94333c77e266129d935b244022d10fce718b5c8f
Moacir Zimermann Junior <moacirjrzimermann@gmail>
2021-04-12 00:20:47 -0300

    Tornando os icones das linguagens dinamicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import Card from './components/Card';
 
-import moacir from './assets/img/moacir.jpeg';
-import tim from './assets/img/tim.png';
+import euImg from './assets/img/moacir.jpeg';
+import timImg from './assets/img/tim.png';
+
+const eu = {
+  img: euImg,
+  langs: ['js', 'css', 'html', 'react', 'angular', 'cpp', 'java', 'db']
+}
+
+const tim = {
+  img: timImg,
+  langs: ['db', 'mongo', 'html', 'css', 'js']
+}
 
 function App() {
   return (
-    <>
-      <Card name="Moacir Zimermann Junior" role="Programador" img={moacir} />
-      <Card name="Tim Bernes-Lee" role="Criador da World Wide Web" img={tim} />
-    </>
+     <>
+       <Card name="Moacir Zimermann Junior" role="Programador" img={eu.img} langs={eu.langs} />
+       <Card name="Tim Bernes-Lee" role="Criador da World Wide Web" img={timImg} langs={tim.langs} />
+     </>
   );
 }
 
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import './Card.scss';
 
 function Card(props) {
-  const {name, role, img} = props;
-  
+  const { name, role, img, langs } = props;
+
+  const getIconClass = lang => {
+    switch(lang) {
+      case 'angular': return 'list-item--angular';
+      case 'cpp': return 'list-item--cpp';
+      case 'css': return 'list-item--css';
+      case 'html': return 'list-item--html';
+      case 'java': return 'list-item--java';
+      case 'js': return 'list-item--js';
+      case 'mongo': return 'list-item--mongo';
+      case 'db': return 'list-item--db';
+      case 'php': return 'list-item--php';
+      case 'python': return 'list-item--python';
+      case 'react': return 'list-item--react';
+      case 'vue': return 'list-item--vue';
+      default: return '';
+    }
+  };
+
   return (
     <div className="card">
       <div className="card__title">
@@ -15,17 +33,14 @@ function Card(props) {
           <div className="card__body__info__knowledge">
             <span className="card__body__info__knowledge__title">Conhecimentos</span>
             <ul className="card__body__info__knowledge__list">
-              <li className="list-item list-item--javascript"></li>
-              <li className="list-item list-item--html"></li>
-              <li className="list-item list-item--css"></li>
-              <li className="list-item list-item--react"></li>
-              <li className="list-item list-item--cpp"></li>
-              <li className="list-item list-item--java"></li>
+              {langs?.map((lang, index) => (
+                <li key={index} className={`list-item ${getIconClass(lang)}`}></li>
+              ))}
             </ul>
           </div>
         </div>
         <div className="card__body__profile-picture">
-          <img className="card__body__profile-picture__img" src={img} />
+          <img className="card__body__profile-picture__img" alt={name} src={img} />
         </div>         
       </div>
     </div>
